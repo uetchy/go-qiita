eb55cebc7b59a4d0ae4b0bcbf613c72140969803
Yasuaki Uechi <uetchy@randompaper.co>
2015-09-17 16:33:02 +0900

    Add all users methods

--- qiita/users_test.go
+++ qiita/users_test.go
@@ -25,39 +25,16 @@ func TestUsersService_Stocks(t *testing.T) {
         "coediting": false,
         "id": "4bd431809afb1bb99e4f",
         "private": false,
-        "tags": [
-          {
-            "name": "Ruby",
-            "versions": [
-              "0.0.1"
-            ]
-          }
-        ],
+        "tags": [],
         "title": "Example title",
         "url": "https://qiita.com/yaotti/items/4bd431809afb1bb99e4f",
-        "user": {
-          "description": "Hello, world.",
-          "facebook_id": "yaotti",
-          "followees_count": 100,
-          "followers_count": 200,
-          "github_login_name": "yaotti",
-          "id": "yaotti",
-          "items_count": 300,
-          "linkedin_id": "yaotti",
-          "location": "Tokyo, Japan",
-          "name": "Hiroshige Umino",
-          "organization": "Increments Inc",
-          "permanent_id": 1,
-          "profile_image_url": "https://si0.twimg.com/profile_images/2309761038/1ijg13pfs0dg84sk2y0h_normal.jpeg",
-          "twitter_screen_name": "yaotti",
-          "website_url": "http://yaotti.hatenablog.com"
-        }
+        "user": {}
       }
     ]`)
 	})
 
 	opt := &ListOptions{Page: 1, PerPage: 2}
-	items, _, err := client.Users.Stocks("yaotti", opt)
+	items, err := client.Users.Stocks("yaotti", opt)
 
 	if err != nil {
 		t.Errorf("Issues.List returned error: %v", err)
@@ -70,31 +47,10 @@ func TestUsersService_Stocks(t *testing.T) {
 			CoEditing:    false,
 			Id:           "4bd431809afb1bb99e4f",
 			Private:      false,
-			Tags: []AttachedTag{{
-				"Ruby",
-				[]string{
-					"0.0.1",
-				},
-			}},
-			Title: "Example title",
-			URL:   "https://qiita.com/yaotti/items/4bd431809afb1bb99e4f",
-			User: User{
-				Description:       "Hello, world.",
-				FacebookId:        "yaotti",
-				FolloweesCount:    100,
-				FollowersCount:    200,
-				GithubLoginName:   "yaotti",
-				Id:                "yaotti",
-				ItemsCount:        300,
-				LinkedinId:        "yaotti",
-				Location:          "Tokyo, Japan",
-				Name:              "Hiroshige Umino",
-				Organization:      "Increments Inc",
-				PermanentId:       1,
-				ProfileImageURL:   "https://si0.twimg.com/profile_images/2309761038/1ijg13pfs0dg84sk2y0h_normal.jpeg",
-				TwitterScreenName: "yaotti",
-				WebsiteURL:        "http://yaotti.hatenablog.com",
-			},
+			Tags:         []AttachedTag{},
+			Title:        "Example title",
+			URL:          "https://qiita.com/yaotti/items/4bd431809afb1bb99e4f",
+			User:         User{},
 		},
 	}
 	if !reflect.DeepEqual(items, want) {
