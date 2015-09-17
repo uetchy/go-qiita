b2e60fcc44229e3f89998e776e869d5c2f2f7ef6
Yasuaki Uechi <uetchy@randompaper.co>
2015-09-17 16:33:32 +0900

    Add all comments methods

--- qiita/comments.go
+++ qiita/comments.go
@@ -17,8 +17,36 @@ type Comment struct {
 	User         User      `json:"user"`
 }
 
-func (s *CommentsService) Get() (*Comment, error) {
-	req, err := s.client.NewRequest("GET", "comments/1", nil)
+// TODO: Post a comment on an item.
+func (s *CommentsService) Create(itemId string, body string) error {
+	req, err := s.client.NewRequest("POST", "items/"+itemId+"/comments", nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: List comments on an item in newest order.
+func (s *CommentsService) List(itemId string) ([]Comment, error) {
+	req, err := s.client.NewRequest("GET", "items/"+itemId+"/comments", nil)
+	if err != nil {
+		return nil, err
+	}
+	comments := new([]Comment)
+	_, err = s.client.Do(req, comments)
+	if err != nil {
+		return nil, err
+	}
+	return *comments, err
+}
+
+// TODO: Get a comment.
+func (s *CommentsService) Get(commentId string) (*Comment, error) {
+	req, err := s.client.NewRequest("GET", "comments/"+commentId, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -28,4 +56,56 @@ func (s *CommentsService) Get() (*Comment, error) {
 		return nil, err
 	}
 	return comment, nil
+}
+
+// TODO: Update a comment.
+func (s *CommentsService) Edit(commentId string, comment *Comment) error {
+	req, err := s.client.NewRequest("PATCH", "comments/"+commentId, nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: Delete a comment.
+func (s *CommentsService) Delete(commentId string) error {
+	req, err := s.client.NewRequest("DELETE", "comments/"+commentId, nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: Send a thank to a comment.
+func (s *CommentsService) AddThank(id string) error {
+	req, err := s.client.NewRequest("PUT", "comments/"+id+"/thank", nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO: Delete a thank from a comment.
+func (s *CommentsService) DeleteThank(id string) error {
+	req, err := s.client.NewRequest("DELETE", "comments/"+id+"/thank", nil)
+	if err != nil {
+		return err
+	}
+	_, err = s.client.Do(req, nil)
+	if err != nil {
+		return err
+	}
+	return nil
 }
